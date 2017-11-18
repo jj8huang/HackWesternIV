7d167f122dd7486cf89a708373a443a358ef3027
John Huang <johnjunhh@gmail.com>
2017-11-18 10:53:42 -0500

    Display text box

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to WEATHER LAND</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-      </div>
+       <div className="App">
+    {/*     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to WEATHER LAND</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <Test />
+    //     <Test />
+    //     <Test />
+    //     <Test />*/}
+    <MuiThemeProvider>
+    <TextFieldExampleSimple >
+    </TextFieldExampleSimple>
+    </MuiThemeProvider>
+       </div>
     );
   }
 }
@@ -31,6 +37,16 @@ class Test extends Component {
     );
   }
 }
+
+
+const TextFieldExampleSimple = () => (
+  <div>
+    <TextField
+      hintText="Location"
+    /><br />
+    <br />    
+  </div>
+);
 
 
 export default App;
