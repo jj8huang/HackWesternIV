c118bcd52a2654b24b9b2ae0a9548e90d6dc65f3
John Huang <johnjunhh@gmail.com>
2017-11-18 13:08:06 -0500

    added button

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -2,49 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
   render() {
     return (
-       <div className="App">
-    {/*     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to WEATHER LAND</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <Test />
-    //     <Test />
-    //     <Test />
-    //     <Test />*/}
-    <MuiThemeProvider>
-    <TextFieldExampleSimple >
-    </TextFieldExampleSimple>
-    </MuiThemeProvider>
-       </div>
+      <div className="App">
+        <header className="weatherHeader">
+        <div className="searchBox">
+            <MuiThemeProvider>
+              <LocationBox>
+              </LocationBox>
+            </MuiThemeProvider>
+            <MuiThemeProvider>
+              <EnterButton>
+              </EnterButton>
+            </MuiThemeProvider>
+          </div>
+        </header>
+      </div>
     );
   }
 }
 
-class Test extends Component {
-  render(){
-    return(
-    <div>
-      <h1>TEEEEEEEEEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSTTTTTTTTTTTTTTTTTTTTTTT</h1>
-    </div>
-    );
-  }
-}
-
-
-const TextFieldExampleSimple = () => (
+const LocationBox = () => (
   <div>
     <TextField
       hintText="Location"
     /><br />
     <br />    
+  </div>
+);
+
+const EnterButton = () => (
+  <div>
+    <FlatButton className="button" label="Enter" />
   </div>
 );
 
